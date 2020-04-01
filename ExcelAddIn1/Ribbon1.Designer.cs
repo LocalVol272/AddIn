87d2821d18e344b91d8e8324aa4d9e294f88101b
Becksou <31825467+Becksou@users.noreply.github.com>
2020-04-01 19:52:57 +0200

    Update Ribbon1.Designer.cs

--- ExcelAddIn1/Ribbon1.Designer.cs
+++ ExcelAddIn1/Ribbon1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ExcelAddIn1
+namespace ExcelAddIn1
 {
     partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
     {
@@ -37,18 +37,24 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ribbon1));
             this.tab1 = this.Factory.CreateRibbonTab();
             this.group1 = this.Factory.CreateRibbonGroup();
-            this.group2 = this.Factory.CreateRibbonGroup();
-            this.comboBox1 = this.Factory.CreateRibbonComboBox();
-            this.Maturity = this.Factory.CreateRibbonEditBox();
-            this.group3 = this.Factory.CreateRibbonGroup();
             this.button2 = this.Factory.CreateRibbonButton();
             this.button1 = this.Factory.CreateRibbonButton();
-            this.Price = this.Factory.CreateRibbonButton();
+            this.group2 = this.Factory.CreateRibbonGroup();
             this.comboBox2 = this.Factory.CreateRibbonComboBox();
+            this.comboBox3 = this.Factory.CreateRibbonComboBox();
+            this.button4 = this.Factory.CreateRibbonButton();
+            this.group3 = this.Factory.CreateRibbonGroup();
+            this.Price = this.Factory.CreateRibbonButton();
+            this.editBox1 = this.Factory.CreateRibbonEditBox();
+            this.editBox2 = this.Factory.CreateRibbonEditBox();
+            this.editBox3 = this.Factory.CreateRibbonEditBox();
+            this.group4 = this.Factory.CreateRibbonGroup();
+            this.button3 = this.Factory.CreateRibbonButton();
             this.tab1.SuspendLayout();
             this.group1.SuspendLayout();
             this.group2.SuspendLayout();
             this.group3.SuspendLayout();
+            this.group4.SuspendLayout();
             this.SuspendLayout();
             // 
             // tab1
@@ -56,43 +62,16 @@
             this.tab1.Groups.Add(this.group1);
             this.tab1.Groups.Add(this.group2);
             this.tab1.Groups.Add(this.group3);
-            this.tab1.Label = "Local Vol Toolkit";
+            this.tab1.Groups.Add(this.group4);
+            this.tab1.Label = "Local Volatility Toolkit";
             this.tab1.Name = "tab1";
             // 
             // group1
             // 
             this.group1.Items.Add(this.button2);
             this.group1.Items.Add(this.button1);
-            this.group1.Label = "First Step";
+            this.group1.Label = "Initialization";
             this.group1.Name = "group1";
-            // 
-            // group2
-            // 
-            this.group2.Items.Add(this.comboBox1);
-            this.group2.Items.Add(this.comboBox2);
-            this.group2.Items.Add(this.Maturity);
-            this.group2.Label = "Parameters";
-            this.group2.Name = "group2";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.Label = "Country";
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Text = null;
-            this.comboBox1.TextChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.comboBox1_TextChanged);
-            // 
-            // Maturity
-            // 
-            this.Maturity.Label = "Maturity";
-            this.Maturity.Name = "Maturity";
-            this.Maturity.Text = null;
-            this.Maturity.TextChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.editBox1_TextChanged);
-            // 
-            // group3
-            // 
-            this.group3.Items.Add(this.Price);
-            this.group3.Label = "Pricing";
-            this.group3.Name = "group3";
             // 
             // button2
             // 
@@ -108,20 +87,85 @@
             this.button1.Label = "";
             this.button1.Name = "button1";
             // 
+            // group2
+            // 
+            this.group2.Items.Add(this.comboBox2);
+            this.group2.Items.Add(this.comboBox3);
+            this.group2.Items.Add(this.button4);
+            this.group2.Label = "Global Description";
+            this.group2.Name = "group2";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Label = "Ticker  ";
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Text = null;
+            this.comboBox2.TextChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.comboBox2_TextChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Label = "Type    ";
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Text = null;
+            this.comboBox3.TextChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.comboBox3_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Label = "Import Data";
+            this.button4.Name = "button4";
+            this.button4.ShowImage = true;
+            this.button4.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button4_Click);
+            // 
+            // group3
+            // 
+            this.group3.Items.Add(this.Price);
+            this.group3.Items.Add(this.editBox1);
+            this.group3.Items.Add(this.editBox2);
+            this.group3.Items.Add(this.editBox3);
+            this.group3.Label = "Volatility Surface";
+            this.group3.Name = "group3";
+            // 
             // Price
             // 
             this.Price.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
             this.Price.Image = ((System.Drawing.Image)(resources.GetObject("Price.Image")));
-            this.Price.Label = "Price";
+            this.Price.Label = "Volatility Surface";
             this.Price.Name = "Price";
             this.Price.ShowImage = true;
             this.Price.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.Price_Click);
             // 
-            // comboBox2
+            // editBox1
             // 
-            this.comboBox2.Label = "Ticker";
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.TextChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.comboBox2_TextChanged);
+            this.editBox1.Label = "     Moneyness    ";
+            this.editBox1.Name = "editBox1";
+            this.editBox1.Text = null;
+            // 
+            // editBox2
+            // 
+            this.editBox2.Label = "     Max Maturity  ";
+            this.editBox2.Name = "editBox2";
+            this.editBox2.Text = null;
+            // 
+            // editBox3
+            // 
+            this.editBox3.Label = "     Grid Accuracy";
+            this.editBox3.Name = "editBox3";
+            this.editBox3.Text = null;
+            // 
+            // group4
+            // 
+            this.group4.Items.Add(this.button3);
+            this.group4.Label = "Pricing";
+            this.group4.Name = "group4";
+            // 
+            // button3
+            // 
+            this.button3.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Label = "Price";
+            this.button3.Name = "button3";
+            this.button3.ShowImage = true;
             // 
             // Ribbon1
             // 
@@ -137,6 +181,8 @@
             this.group2.PerformLayout();
             this.group3.ResumeLayout(false);
             this.group3.PerformLayout();
+            this.group4.ResumeLayout(false);
+            this.group4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -148,11 +194,16 @@
         internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
         internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
-        internal Microsoft.Office.Tools.Ribbon.RibbonComboBox comboBox1;
-        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox Maturity;
         internal Microsoft.Office.Tools.Ribbon.RibbonGroup group3;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton Price;
         internal Microsoft.Office.Tools.Ribbon.RibbonComboBox comboBox2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonComboBox comboBox3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group4;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button4;
     }
 
     partial class ThisRibbonCollection
